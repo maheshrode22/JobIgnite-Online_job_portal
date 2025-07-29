67071f4d06ad35fd14e4f7c9c1f3129d6ca8852e
VaibhavMagar1504 <vaibhavmagar2222@gmail.com>
2025-07-29 09:42:27 +0530

    hr login

--- src/models/jobseekerModel.js
+++ src/models/jobseekerModel.js
@@ -4,7 +4,6 @@ exports.jobSeekerLogin=(jobUser,jobPass)=>{
 
     return new Promise((resolve,reject)=>{
         db.query("select * from job_seekers where email=? AND password=?",[jobUser,jobPass],(err,result)=>{
-            
             if(err)
             {
                 reject(err);

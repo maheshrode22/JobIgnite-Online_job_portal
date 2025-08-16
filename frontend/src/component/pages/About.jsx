9622bb76d679aa28a5967e7f18a6c5e2fbef94eb
VaibhavMagar1504 <vaibhavmagar2222@gmail.com>
2025-08-16 10:23:56 +0530

    home page

--- frontend/src/component/pages/About.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function About() {
-  return <h2>ℹ️ About Our Website</h2>;
-}
--- frontend/src/component/pages/Contact.jsx
+++ frontend/src/component/pages/Contact.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Contact() {
-  return <h2>📞 Contact Us</h2>;
+  return (
+  <>
+      <h2>Contact US</h2>
+      <input type="text" name="name" id="" />
+      <input type="text" name="email" id="" />
+  </>
+  );
+
 }
